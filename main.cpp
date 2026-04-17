24b3ff44324ad4f01854cdb1094cc7a79a4e99bf
Ark313-sd <saad.tahsin@gmail.com>
2026-04-17 15:08:12 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,17 @@
-#include <iostream>
-#include <vector>
-#include "config.hpp"
-#include "Controller.hpp"
-#include "Cache.hpp"
-using namespace std;
-
-
-
-
 int main()
 {
-    // need only be created once
-    // createRAM();
+    createRAM();   // for now, keep it enabled while testing
 
-
-    // cf.line[0]->setMetadata("00000000", true, false);
-    // vector<string> data = {"10110011100011110000111100110011", "0000", "1111", "1010"};
-    // cf.line[0]->insertData(data);
-    // cf.printCache();
-    
-    Cache* cache = new Cache(3, 2);
+    Cache* cache = new Cache(3, 2);   // 2^3 = 8 lines, 2^2 = 4 words per line
     Controller c(cache);
-    c.addInstruction(Instruction(Instruction::addWord, "0", "10000"));
-    c.addInstruction(Instruction(Instruction::addByte, "1", "10101"));
+
+    c.addInstruction(Instruction(Instruction::addWord, "0000000000000000", "0000000000010000"));
+    c.addInstruction(Instruction(Instruction::addByte, "0000000000000001", "0000000000010001"));
+
     c.operate();
 
+    c.evictCache();   // flush dirty lines before program ends
 
-
-
+    delete cache;
     return 0;
 }
